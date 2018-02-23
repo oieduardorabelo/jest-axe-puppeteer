b2e5e608a5af4fdc052b3799448c3bb9c2732a44
Eduardo Rabelo <oieduardorabelo@gmail.com>
2018-02-24 12:55:04 +1300

    feat: add integration/setup and express as dep

---
+++ integration/config/setup.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+const puppeteer = require("puppeteer");
+const mkdirp = require("mkdirp");
+const express = require("express");
+const DIR = path.join(os.tmpdir(), "jest_puppeteer_global_setup");
+const IS_WATCH = process.argv.includes("--watch");
+module.exports = async () => {
+  // [A]
+  global.BROWSER = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  });
+  // [B]
+  mkdirp.sync(DIR);
+  fs.writeFileSync(path.join(DIR, "wsEndpoint"), global.BROWSER.wsEndpoint());
+  // [C]
+  if (!IS_WATCH) {
+    const app = express();
+    app.use("/", express.static(path.join("public")));
+    global.SERVER = app.listen(5000);
+  }
+};
