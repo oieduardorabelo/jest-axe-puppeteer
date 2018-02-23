8c53109f1f4d41376ce5167ef5915a0867a1c6ea
Eduardo Rabelo <oieduardorabelo@gmail.com>
2018-02-24 12:32:17 +1300

    feat: add integration/ and jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  globalSetup: "./integration/config/setup.js",
+  globalTeardown: "./integration/config/teardown.js",
+  testEnvironment: "./integration/config/puppeteer_environment.js"
+};
